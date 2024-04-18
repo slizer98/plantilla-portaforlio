6867a518152cfdf68bc14ff85cdb0aef9aeb4515
slizer98 <erick.m.villa.r@gmail.com>
2024-04-18 17:22:06 -0600

    Add migration file for altering project options and project link in portfolio app

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,9 +5,9 @@ class Project(models.Model):
   title = models.CharField(max_length=200, verbose_name="Titulo")
   description = models.TextField(verbose_name="Descripción")
   image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+  link = models.URLField(verbose_name="Dirección Web" ,null=True, blank=True)
   created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
   updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edicón")
-
 
   class Meta:
     verbose_name = "proyecto"
