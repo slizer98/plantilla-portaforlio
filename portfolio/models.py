f70a7be78963be3173b55456595a18d836b15c1e
slizer98 <erick.m.villa.r@gmail.com>
2024-04-18 11:55:31 -0600

    deleted webpersonal_frontend

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,3 +7,7 @@ class Project(models.Model):
   image = models.ImageField()
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
+
+
+  class Meta:
+    verbose_name = "Proyecto"
